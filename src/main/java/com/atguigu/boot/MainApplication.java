820377aac2552e991a1dbcf21366be124cffdce1
codesky188 <703616834@qq.com>
2023-04-17 15:43:13 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/boot/MainApplication.java
@@ -0,0 +1,16 @@
+package com.atguigu.boot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+* 主程序类
+* @SpringBootApplication：这是一个Springboot应用
+* */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
